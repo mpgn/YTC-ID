43ae498b61e6ac4ecf9a7fc7ba9ea94669886395
Kévin Barreau <kevin.barreau.info@gmail.com>
2014-08-24 11:00:29 +0200

    Add img thumbnail & less API call
    Img thumbnail can help to find visually if the user make an error in the
    channel name. Refactor code in gaspi.js to avoid some calls to API.

--- js/gapsi.js
+++ js/gapsi.js
@@ -1,23 +1,36 @@
 function googleApiClientReady() {
-    var apiKey = 'YOUR_API_KEY';
-    var youtubeId;
+    var apiKey = 'YOUR_API_KEY',
+        isLoad = false,
+        username = "",
+        oldUsername = "",
+        youtubeId,
+        request;
+    gapi.client.setApiKey(apiKey);
+    gapi.client.load('youtube', 'v3', function() {
+        isLoad = true;
+    }); 
     $('#input-youtube').keyup(function() {
-        var username = $('input[name="channelname"]').val();
-        gapi.client.setApiKey(apiKey);
-        gapi.client.load('youtube', 'v3', function() {
-            var request = gapi.client.youtube.channels.list({
-                part: 'id',
+        username = $('input[name="channelname"]').val();
+        //call API only if input changed and API loaded
+        if (isLoad && (username !== oldUsername)) {
+            oldUsername = username;
+            $('#thumbnail-channel').addClass("empty");
+            request = gapi.client.youtube.channels.list({
+                part: 'id,snippet',
                 forUsername : username
             });
             request.execute(function(response) {
                 if(response.pageInfo.totalResults != 0) {
                     youtubeId = response.result.items[0].id;
                     $('.id').text(youtubeId);
+                    $('#thumbnail-channel').attr("src", response.result.items[0].snippet.thumbnails.default.url);
+                    $('#thumbnail-channel').removeClass("empty");
                 } else {
                     $('.id').text(" ");
+                    $('#thumbnail-channel').attr("src", " ");
                 }
             });
-        }); 
+        }
     })
     .keyup();
 }
